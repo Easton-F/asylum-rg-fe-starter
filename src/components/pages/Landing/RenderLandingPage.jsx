d520007030bb062c41b915788030122829a48466
Easton <easton.fritsch21@gmail.com>
2023-11-15 19:22:20 -0700

    finished bottom section in landing page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -80,8 +80,41 @@ function RenderLandingPage(props) {
         </div>
       </div>
       <div>
-        {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className="bottom-section-info">
+            <h1>36%</h1>
+            <p>
+              By the end of the Trump administration, the average asylum office
+              grant rate had fallen 36 percent from an average of 44 percent in
+              fiscal year 2016 to 28 percent in fiscal year 2020.
+            </p>
+          </div>
+          <div className="bottom-section-info">
+            <h1>5%</h1>
+            <p>
+              The New York asylum office grant rate dropped 5 percent in fiscal
+              year 2020.
+            </p>
+          </div>
+          <div className="bottom-section-info">
+            <h1>6x Lower</h1>
+            <p>
+              Between fiscal year 2017 and 2020, the New York asylum office's
+              average grant rate was six times lower than the San Francisco
+              asylum office.
+            </p>
+          </div>
+          <div className="read-more-btn">
+            <Button
+              type="default"
+              style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+              // onClick={() => history.push('/graphs')}
+            >
+              Read More
+            </Button>
+          </div>
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
