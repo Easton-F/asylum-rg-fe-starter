5c39785a9f0c7a6910da13c8921e666823f341d5
Easton <easton.fritsch21@gmail.com>
2023-12-01 14:24:41 -0700

    fixed auth-nav.js

--- src/components/common/auth-nav.js
+++ src/components/common/auth-nav.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import AuthenticationButton from './authentication-button';
 
-import LoginButton from './login-button';
-import LogoutButton from './logout-button';
+const AuthNav = () => (
+  <div className="navbar-nav ml-auto">
+    <AuthenticationButton />
+  </div>
+);
 
-import { useAuth0 } from '@auth0/auth0-react';
-
-const AuthenticationButton = () => {
-  const { isAuthenticated } = useAuth0();
-
-  return isAuthenticated ? <LogoutButton /> : <LoginButton />;
-};
-
-export default AuthenticationButton;
+export default AuthNav;
